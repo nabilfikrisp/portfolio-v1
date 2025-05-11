26ea1228b8c72859dd76eb168898071620095c4c
nabilfikrisp <79518623+nabilfikrisp@users.noreply.github.com>
2025-05-11 14:59:09 +0700

    Fix/responsive (#2)
    * feat: added animation on my apps
    
    * feat: update footer name and adjust layout in education and work experience cards

--- src/components/sections/tabs/WorkExperienceCards.tsx
+++ src/components/sections/tabs/WorkExperienceCards.tsx
@@ -3,7 +3,7 @@ import { cn } from "@/lib/utils";
 
 export function WorkExperienceCards() {
   return (
-    <div className="mt-4 flex flex-col overflow-hidden rounded border border-my-accent-one px-5">
+    <div className="mt-2 flex flex-col overflow-hidden rounded border border-my-accent-one px-2 lg:mt-4 lg:px-5">
       {WORKS.map((work, index) => (
         <WorkExperienceCard
           key={index}
@@ -40,7 +40,7 @@ export function WorkExperienceCard({
       >
         <div className="absolute right-1/2 top-0 -z-10 h-full w-[2px] bg-my-accent-one text-transparent" />
         <img
-          className="z-10 aspect-square h-20 w-20 overflow-hidden rounded border border-my-accent-one bg-my-background object-scale-down"
+          className="z-10 aspect-square h-16 w-16 overflow-hidden rounded border border-my-accent-one bg-my-background object-scale-down lg:h-20 lg:w-20"
           src={work.imageUrl}
         />
       </div>
@@ -56,18 +56,27 @@ export function WorkExperienceCard({
         <h2 className="text-xl font-semibold text-my-accent-one">
           {work.title}
         </h2>
-        <div className="flex gap-2 text-my-paragraph/70">
-          <span>{work.company}</span>
-          <span>|</span>
-          <span>{work.employmentType}</span>
-          <span>|</span>
-          <span>{work.date}</span>
+        <div className="flex flex-col text-my-paragraph/70 lg:flex-row lg:gap-2">
+          <span>
+            <span className="me-1 inline-block lg:hidden">-</span>
+            {work.company}
+          </span>
+          <span className="hidden lg:block">|</span>
+          <span>
+            <span className="me-1 inline-block lg:hidden">-</span>
+            {work.employmentType}
+          </span>
+          <span className="hidden lg:block">|</span>
+          <span>
+            <span className="me-1 inline-block lg:hidden">-</span>
+            {work.date}
+          </span>
         </div>
         <p>{work.description}</p>
         {work.descriptionList && (
           <ul>
             {work.descriptionList.map((text) => (
-              <li className="text-my-paragraph/75" key={text}>
+              <li className="text-balance text-my-paragraph/75" key={text}>
                 - {text}
               </li>
             ))}
